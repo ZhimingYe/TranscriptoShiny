bcc4c12d1fe827bbafa02809a54a561321978968
ZhimingYe <zhiming.ye@qq.com>
2024-08-14 04:20:11 +0800

    Finish UI

--- DEGAnalysis/app.R
+++ DEGAnalysis/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinyjs)
+library(tibble)
 library(bslib)
 library(DESeq2)
 library(ggplot2)
@@ -10,9 +11,49 @@ ui <- fluidPage(
   titlePanel("Differential Expression Analysis"),
   sidebarLayout(
     sidebarPanel(
+      div(style = "text-align: center;",
+          tags$footer(paste0("Version 1.0 Author: Zhiming Ye"),
+                      style = "font-size: 12px; color: grey;")),
+      div(style = "text-align: center;",
+          tags$footer(paste0("@ YAO LAB"),
+                      style = "font-size: 12px; color: grey;")),
+      passwordInput("passwd", "Password"),
+      actionButton("loginBtn", "Login"),
+      hr(),
+      div(style = "text-align: center;",
+          tags$footer(paste0("Please save as CSV using Excel (File->save as comma seperated)"),
+                      style = "font-size: 12px; color: black;")),
+      div(style = "text-align: center;",
+          tags$footer(paste0("WARNING: DONOT use UTF8 csv!"),
+                      style = "font-size: 15px; color: red;")),
+      div(style = "text-align: center;",
+         tags$footer(paste0("Row: Gene; Column: Sample, Sample name MUST BE unique"),
+                     style = "font-size: 15px; color: red;")),
       fileInput("countMatrix", "Upload Expression Matrix (CSV)", accept = "text/csv"),
+      div(style = "text-align: center;",
+          tags$footer(paste0("Group Info contains TWO columns, the first is sample ID, should be same as expression matrix, the second is grouping information. Please don't add any other columns"),
+                      style = "font-size: 15px; color: red;")),
       fileInput("colData", "Upload Group Information (CSV)", accept = "text/csv"),
-      selectInput("selectA", "Select Comparasion", choices = NULL),
+      radioButtons("radio", "Matrix Type",
+                   choices = list("Counts" = 1, "TPM" = 2, "Not from Sequencing" = 3), 
+                   selected = 1),
+      sliderInput("sliderRows", "Genes expressed in N sample were keet", min = 1, max = 1, value = 1),
+      checkboxInput("checkbox","Protein coding gene only (only supports human and mouse)", value = F),
+      checkboxInput("checkbox","NCRNA only (only supports human and mouse)", value = F),
+      hr(),
+      radioButtons("radio", "Analysis Type",
+                   choices = list("DEA according to Group" = 1, "DEA according to Trend" = 2), 
+                   selected = 1),
+      selectInput("selectA", "If according to Group, Select Comparasion", choices = NULL),
+      selectInput("selectB", "If according to Trend, Select Order", choices = NULL),
+      sliderInput("sliderRows", "If according to Trend, Select Cluster numbers", min = 1, max = 10, value = 1),
+      checkboxInput("checkbox","If according to Trend, Perform prefiltering based on DEA?", value = TRUE),
+      checkboxInput("checkbox","If according to Trend, adjust P?", value = TRUE),
+      hr(),
+      checkboxInput("checkbox","Add gene annotation", value = TRUE),
+      div(style = "text-align: center;",
+          tags$text(paste0("Annotation only supports human and mouse."),
+                      style = "font-size: 12px; color: black;")),
       actionButton("runDESeq", "Run DEG analysis"),
       downloadButton("downloadData", "Download Differential Genes CSV")
     ),
@@ -21,7 +62,8 @@ ui <- fluidPage(
         nav_panel("PCA", plotOutput("PCAplot")),
         nav_panel("Pearson Correlation", plotOutput("PCCCR")),
         nav_panel("DEG analysis",plotOutput("volcanoPlot")),
-        nav_panel("DEG table",DTOutput("table"))
+        nav_panel("DEG table",DTOutput("table")),
+        nav_panel("sessionInfo",DTOutput("SesionInfo")),full_screen=T,wrapper=card_body(height="1200px")
       )
     )
   )
@@ -84,13 +126,18 @@ server <- function(input, output, session) {
     }
     
   })
-  
+  output$SesionInfo<-renderDT({
+    (xfun::session_info()%>%as_tibble())[-c(1:3),]
+  }, options = list(pageLength = 50, scrollX = T)
+    
+  )
   output$downloadData <- downloadHandler(
       filename = function() { "differential_genes.csv" },
       content = function(file) {
         write.csv(as.data.frame(resOrdered), file)
       }
   )
+  
   
   
   
